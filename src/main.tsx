68f22a18eaca75f9c993e8b2ddd76ce7fde6d99c
ogois-rp <tiagoppal@gmail.com>
2023-06-26 21:13:45 +0100

    Main page.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom/client'
-import App from './App'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { SoccerScore } from "./components/SoccerScore";
+import { results } from "./data/data";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<React.StrictMode>
+		<SoccerScore results={results} />
+	</React.StrictMode>
+);
