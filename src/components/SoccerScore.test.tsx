2ac81c5ee84e3b383268d45cd9effd3ccf29997a
ogois-rp <tiagoppal@gmail.com>
2023-06-26 22:56:05 +0100

    Refactor testing to accomodate store.

--- src/components/SoccerScore.test.tsx
+++ src/components/SoccerScore.test.tsx
@@ -1,22 +1,25 @@
-import "@testing-library/jest-dom"
-import { render } from "@testing-library/react"
-import { describe, test } from "vitest"
-import { SoccerScore } from "./SoccerScore"
-import { results } from 'src/data/data'
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import store from "src/redux/store/store";
+import { describe, test } from "vitest";
+import { SoccerScore } from "./SoccerScore";
 
-describe("Soccer Score", () => {
-  test("Should show the scores on the screen", () => {
-    const data = results
+describe("Soccer Score Component Testing Suite", () => {
+	test("Should show the scores on the screen", () => {
+		const { getByText } = render(
+			<Provider store={store}>
+				<SoccerScore />
+			</Provider>
+		);
 
-    const { getByText } = render(<SoccerScore results={data} />)
-
-    expect(getByText('Austria')).toBeInTheDocument();
-    expect(getByText('Belgium')).toBeInTheDocument();
-    expect(getByText('Ukraine')).toBeInTheDocument();
-    expect(getByText('England')).toBeInTheDocument();
-    expect(getByText('Senegal')).toBeInTheDocument();
-    expect(getByText('South Africa')).toBeInTheDocument();
-    expect(getByText('Belarus')).toBeInTheDocument();
-    expect(getByText('Argentina')).toBeInTheDocument();
-  })
-})
+		expect(getByText("Austria")).toBeInTheDocument();
+		expect(getByText("Belgium")).toBeInTheDocument();
+		expect(getByText("Ukraine")).toBeInTheDocument();
+		expect(getByText("England")).toBeInTheDocument();
+		expect(getByText("Senegal")).toBeInTheDocument();
+		expect(getByText("South Africa")).toBeInTheDocument();
+		expect(getByText("Belarus")).toBeInTheDocument();
+		expect(getByText("Argentina")).toBeInTheDocument();
+	});
+});
