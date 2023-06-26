99a12e86856f90923a7bf083b206c344d36d5072
ogois-rp <tiagoppal@gmail.com>
2023-06-26 22:55:37 +0100

    Add id to game type.

--- src/data/data.ts
+++ src/data/data.ts
@@ -2,24 +2,28 @@ import { Game } from "src/types/types";
 
 const results: Game[] = [
 	{
+		id: "1",
 		home_score: 2,
 		home_team: "Austria",
 		away_team: "Belgium",
 		away_score: 0,
 	},
 	{
+		id: "3",
 		home_score: 2,
 		home_team: "Ukraine",
 		away_team: "England",
 		away_score: 0,
 	},
 	{
+		id: "32",
 		home_score: 2,
 		home_team: "Senegal",
 		away_team: "South Africa",
 		away_score: 0,
 	},
 	{
+		id: "12",
 		home_score: 2,
 		home_team: "Belarus",
 		away_team: "Argentina",
