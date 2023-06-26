7c29a4d439923f23800f8f5b2813d1eeacee68bf
ogois-rp <tiagoppal@gmail.com>
2023-06-26 21:07:19 +0100

    Mock game data.

---
+++ src/data/data.ts
@@ -0,0 +1,30 @@
+import { Game } from "src/types/types";
+
+const results: Game[] = [
+	{
+		home_score: 2,
+		home_team: "Austria",
+		away_team: "Belgium",
+		away_score: 0,
+	},
+	{
+		home_score: 2,
+		home_team: "Ukraine",
+		away_team: "England",
+		away_score: 0,
+	},
+	{
+		home_score: 2,
+		home_team: "Senegal",
+		away_team: "South Africa",
+		away_score: 0,
+	},
+	{
+		home_score: 2,
+		home_team: "Belarus",
+		away_team: "Argentina",
+		away_score: 0,
+	},
+];
+
+export {results}
