a45ddb3230aab81f249942d9b8125c0425683fd4
ogois-rp <tiagoppal@gmail.com>
2023-06-27 23:53:26 +0100

    Component changes passing 2 first tests.

--- src/components/SoccerScore.tsx
+++ src/components/SoccerScore.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { Game } from "src/types/types";
-
+import styles from "./SoccerScore.module.css";
 
 type Props = {
 	results: Game[];
-}
+};
 
-const SoccerScore: React.FC<Props> = ({results}) => {
-
+const SoccerScore: React.FC<Props> = ({ results }) => {
 	return (
 		<ul>
 			{results.map((result) => {
 				return (
-					<li key={result.id}>
-						<span>{`${result.home_team} : ${result.home_score} - ${result.away_team} : ${result.away_score}`}</span>
+					<li className={styles.gameListItem} key={result.id}>
+						<span
+							data-testid={`game-${result.id}`}
+						>{`${result.home_team} : ${result.home_score} - ${result.away_team} : ${result.away_score}`}</span>
+						<button data-testid={`button-${result.id}`} onClick={() => ({})}>
+							End Game
+						</button>
 					</li>
 				);
 			})}
