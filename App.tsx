839f4bfba231a8711f39d4c17ecdfa049354d468
ogois-rp <tiagoppal@gmail.com>
2023-06-27 22:48:53 +0100

    Remove redux from project.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,8 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { SoccerScore } from "./src/components/SoccerScore"
-import { get } from "./src/redux/games/games-slice";
-import { Game } from "./src/types/types";
+import { SoccerScore } from "./src/components/SoccerScore";
+import { results } from "./src/data/data";
 
 const App: React.FC = () => {
-  const dispatch = useDispatch();
-	const games = useSelector((state: Game[]) => state);
+	return <SoccerScore results={results} endGame={() => ({})} />;
+};
 
-	useEffect(() => {
-		dispatch(get());
-	}, [dispatch]);
-
-  return(
-    <SoccerScore results={games}/>
-  )
-}
-
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import store from "../src/redux/store/store";
-import { Provider } from "react-redux";
 import App from "../App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<App/>
-		</Provider>
+		<App />
 	</React.StrictMode>
 );
