9c0da535c999647aa0ca4cd37bdf4e13a73d002c
ogois-rp <tiagoppal@gmail.com>
2023-06-27 23:52:57 +0100

    second test scenario.

--- src/components/SoccerScore.test.tsx
+++ src/components/SoccerScore.test.tsx
@@ -1,6 +1,5 @@
-import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
-import { describe, test } from "vitest";
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { SoccerScore } from "./SoccerScore";
 
 describe("Soccer Score Component Testing Suite", () => {
@@ -14,8 +13,39 @@ describe("Soccer Score Component Testing Suite", () => {
 				away_score: 1,
 			},
 		];
-		const { getByText } = render(<SoccerScore results={games} />);
+		const { getByText } = render(
+			<SoccerScore results={games} />
+		);
 
 		expect(getByText("Portugal : 1 - Morocco : 1")).toBeInTheDocument();
 	});
-});
+
+	test("End Game Button should display on hover", () => {
+		const games = [
+			{
+				id: "ce23",
+				home_team: "Portugal",
+				home_score: 1,
+				away_team: "Morocco",
+				away_score: 1,
+			},
+			{
+				id: "ce1223",
+				home_team: "Argentina",
+				home_score: 1,
+				away_team: "Morocco",
+				away_score: 1,
+			},
+		];
+		render(<SoccerScore results={games} />);
+
+		screen.debug()
+
+    const listItemElement = screen.getByTestId("game-ce23");
+    fireEvent.mouseEnter(listItemElement);
+
+    const buttonElement = screen.getByTestId("button-ce23");
+
+    expect(buttonElement).toHaveStyle("display: inline-block");
+	});
+})
