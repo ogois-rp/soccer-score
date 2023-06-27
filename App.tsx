be9b14b9c7cda91d85ebbbf501d497aaad16162b
ogois-rp <tiagoppal@gmail.com>
2023-06-27 23:51:18 +0100

    Remove endGame function as prop.

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { SoccerScore } from "./src/components/SoccerScore";
 import { results } from "./src/data/data";
 
 const App: React.FC = () => {
-	return <SoccerScore results={results} endGame={() => ({})} />;
+	return <SoccerScore results={results} />;
 };
 
 export default App;
